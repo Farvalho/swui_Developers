9b3c7714578f2401d0cb39053ad033aabf99acd6
Fabio Carvalho <farvalho@outlook.com>
2022-09-05 15:27:34 +0100

    +Add: Default project

---
+++ Developers/Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Fábio Carvalho on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
